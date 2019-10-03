f22da65fafcfd29c37ece3792f247504a432cfa7
Rachel Anderson <rachelsa@princeton.edu>
2019-10-02 21:24:02 -0400

    almost done

--- Code/Estimation/ahl19.R
+++ Code/Estimation/ahl19.R
@@ -6,6 +6,7 @@ run_ahl <- function(data){
   
   # use nonparam regression to smooth Y values
   nonparam <- lm(data$y~data$f_name + data$l_name)
+  sig2.v <- sum(nonparam$residuals^2)/(N-3)
   
   newData <- data %>% group_by(id_x) %>% 
     add_predictions(nonparam, var="gHat") %>%
@@ -13,11 +14,24 @@ run_ahl <- function(data){
            smoothY = sumY - (L-1)*gHat) %>%
     distinct(sumY, id_x, .keep_all = TRUE)
   
+  N <- nrow(newData)
+  
+  # run first stage to estimate var(eps)
+  first.stage <- lm(smoothY ~ x1 + x2, data=newData)
+  sig2.eps <- sum(first.stage$residuals^2)/(N-3)
+  
   # run diff regressions
-  ahl <- lm(smoothY ~ x1, data = newData)
+  newData <- newData %>% mutate(sigX = sig2.eps + (L-1)*sig2.v)
+  X <- cbind(rep(1, nrow(newData)), newData$x1/(newData$sigX), newData$x2/sqrt(newData$sigX))
+  y <- newData$smoothY/sqrt(newData$sigX)
+  beta.ahl <- solve(t(X)%*%X)%*%t(X)%*%y
+  sig <- t(y-X%*%beta.ahl)%*%(y-X%*%beta.ahl)/(N-5)
+  
+  wls <- lm(smoothY ~ x1+x2, weights = sigX, data=newData)
+  se <- sqrt(diag(vcov(wls)))
   # trueMatches <- lm(y ~ x1, data = data %>% filter(true_match==TRUE))
   # need to fix the standard errors
-  return(ahl)
+  return(list(betas = wls$coefficients, se = se))
 }
 
 
--- Code/Estimation/all_estimation.R
+++ Code/Estimation/all_estimation.R
@@ -5,7 +5,7 @@ source("~/Desktop/3rdYrPaper/Code/Estimation/ahl19.R")
 source("~/Desktop/3rdYrPaper/Code/Estimation/lahiri_larsen.R")
 
 dataDir <- "~/Desktop/3rdYrPaper/Code/Data/MatchedData/"
-
+outDir <- "~/Desktop/3rdYrPaper/Figures/"
 ############ Import data and fix variable names ###############
 setwd(dataDir)
 myFiles <- list.files(pattern=".*csv")
@@ -24,6 +24,18 @@ for (f in myFiles){
   rownames(estimates) <- NULL
 }
 
+############ Make a nice table  ###############
+
+addtorow <- list()
+addtorow$pos <- list(0)
+addtorow$command <- c(paste0("& \\multicolumn{4}{c}{AHL} & \\multicolumn{4}{c}{SW} & \\multicolumn{4}{c}{LL} \\\\\n
+                             \cmidrule(lr){2-6}\cmidrule(lr){7-11}\cmidrule(lr){12-16} Parameter",
+                    paste(rep("& ABE-M & ABE-S & PRL-M & PRL-S",3), sep="",collapse=""), "\\\\\n"))
+
+writeLines(capture.output(print(xtable(make_table(estimates)), add.to.row=addtorow, floating=FALSE, include.colnames=FALSE, 
+                                include.rownames=FALSE)),
+           paste0(outDir,"estimates.tex"))
+
 ##########################################################################################
 ############ Helper function for running all estimation procedures ###############
 ##########################################################################################
@@ -32,15 +44,13 @@ estimate_everything <- function(data){
   fileName <- as.character(strsplit(f, '.csv')[1])
   ############### AHL (2019) ###############
   ahl <- run_ahl(data)
-  # estOut <- rbind(estOut, data.frame(est_method = "ahl", dataset = fileName, beta1 = ahl$coefficients[2], se = 0))
+  estOut <- rbind(estOut, make_rows("ahl", fileName, ahl$betas, ahl$se))
   ############### Larsen (2005) ###############
   ll <- lahiri_larsen(data)
   estOut <- rbind(estOut, make_rows("ols", fileName, ll$beta_n$beta, ll$beta_n$se))
   estOut <- rbind(estOut, make_rows("sw", fileName, ll$beta_sw$beta, ll$beta_sw$se))
   estOut <- rbind(estOut,  make_rows("ll", fileName, ll$beta_ll$beta, ll$beta_ll$se))
   ############### Goldstein ###############
-  
-  
   
   return(estOut)
 }
@@ -49,29 +59,37 @@ make_rows <- function(method_name, file, betas, se){
   rows <- data.frame()
   for (i in 1:length(betas)){
     rows <- rbind(rows, data.frame(est_method = method_name, 
-                           matching = file, 
-                           param = paste0("beta", i), 
-                           value = betas[i], 
-                           se=se[i]))
+                                   matching = file, 
+                                   param = paste0("beta", i), 
+                                   value = betas[i], 
+                                   se=se[i]))
   }
   return(rows)
 }
 
+##########################################################################################
+############ Helper functions for printing estimates          ###############
+##########################################################################################
 
 make_table <- function(estimates){
-  params <- c("beta1", "beta2", "beta3")
-
-  
-  estimates %>% filter(param == x & matching == ) %>% 
-  
+  methodList <- as.matrix(c("ahl", "sw", "ll"))
+  cols <- apply(methodList, 1, make_table_row)
+  table <- do.call("cbind", cols)
+  table <- cbind(c("$\\beta_1$", "", "$\\beta_2$", "", "$\\beta_3", ""),table)
+  return(table)
 }
-
-make_table_row <- function(x){
-  dataList <- c("abe_single", "abe_multi", "prl_single", "prl_multi")
-  apply(dataList, )
- 
+make_table_row <- function(m){
+  params <- as.matrix(c("beta1", "beta2", "beta3"))
+  l <- apply(params, 1, make_table_entry, m)
+  rows <- do.call("rbind",l) 
+  return(rows[,2:5])
 }
-make_table_entry <- function(x){
-  estimates %>% filter(param == "beta1" & matching == ) %>% select()
+make_table_entry <- function(x,method){
+  entry <- data.frame()
+  entry <- rbind(entry, estimates %>% filter(est_method == method & param == x) %>% select(-se) %>% spread(matching, value))
+  entry <- rbind(entry, estimates %>% filter(est_method == method & param == x) %>% select(-value) %>% mutate(param="se") %>% spread(matching, se))
+  colnames(entry)[2] <- method
+  entry <- entry %>% select(-est_method)
+  return(entry)
 }
 
