9f095d245f68ee8cbb431617da21456db3ef8a47
Rachel Anderson <rachelsa@princeton.edu>
2019-10-10 08:49:28 -0400

    saving from yesterday

--- Code/Estimation/ahl19.R
+++ Code/Estimation/ahl19.R
@@ -15,16 +15,9 @@ run_ahl <- function(data){
   
   # run diff regressions
   newData <- newData %>% mutate(sigX = sig2.eps + (L-1)*sig2.v)
-  # X <- cbind(rep(1, nrow(newData)), newData$x1/(newData$sigX), newData$x2/sqrt(newData$sigX))
-  # y <- newData$smoothY/sqrt(newData$sigX)
-  # beta.ahl <- solve(t(X)%*%X)%*%t(X)%*%y
-  # sig <- t(y-X%*%beta.ahl)%*%(y-X%*%beta.ahl)/(N-5)
-  
-  wls <- lm(smoothY ~ x1+x2, weights = sigX, data=newData)
+  wls <- lm(smoothY ~ x1+x2, weights = 1/sigX, data=newData)
   se <- sqrt(diag(vcov(wls)))
-  # trueMatches <- lm(y ~ x1, data = data %>% filter(true_match==TRUE))
-  # need to fix the standard errors
-  return(list(betas = first.stage$coefficients, se = sqrt(diag(vcov(first.stage)))))
+  return(list(beta = wls$coefficients, se = se))
 }
 
 
